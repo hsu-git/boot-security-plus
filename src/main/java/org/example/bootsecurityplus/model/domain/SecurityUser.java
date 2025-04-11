a8dad712121656e47e7c68c4190086d8ff7a8a70
hsu-git <hsu080206@gmail.com>
2025-04-11 14:23:42 +0900

    중간 커밋

--- src/main/java/org/example/bootsecurityplus/model/domain/SecurityUser.java
+++ src/main/java/org/example/bootsecurityplus/model/domain/SecurityUser.java
@@ -1,13 +1,11 @@
 package org.example.bootsecurityplus.model.domain;
 
-import java.time.LocalDateTime;
-
 public record SecurityUser(
         Long userId, // null
         String username,
         String password,
         String role,
-        LocalDateTime createdAt // null
+        String createdAt // null
 ) {
     public static SecurityUser toDB(String username, String password, String role) {
         return new SecurityUser(null, username, password, role, null);

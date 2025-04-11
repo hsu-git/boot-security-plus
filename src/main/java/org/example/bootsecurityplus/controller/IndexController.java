eae8dfb8d1ce0e29726e72c822d342dd5905d0c5
hsu-git <hsu080206@gmail.com>
2025-04-11 11:34:33 +0900

    Security 체험

--- src/main/java/org/example/bootsecurityplus/controller/IndexController.java
+++ src/main/java/org/example/bootsecurityplus/controller/IndexController.java
@@ -8,7 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("msg", "Hello World!");
+        model.addAttribute("msg", "로그인이 아닙니다!");
+        return "index";
+    }
+
+//    admin 페이지랑 유저페이지 분화부터
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("msg", "Hello Admin!");
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model) {
+        model.addAttribute("msg", "Hello User!");
         return "index";
     }
 }
